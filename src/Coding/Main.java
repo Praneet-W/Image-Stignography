319dd9e6ffaad1039133eeff97ce9ca7c88caec1
Your Name <you@example.com>
2022-04-29 13:43:56 +0530

    First Commit

---
+++ src/Coding/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Coding;
+
+/**
+ *
+ * @author Prachi
+ */
+public class Main {
+    public static void main(String[] args){
+      MainMenu mm =new MainMenu();
+        mm.setVisible(true);
+        mm.setLocationRelativeTo(null);
+        
+    }
+    
+}
